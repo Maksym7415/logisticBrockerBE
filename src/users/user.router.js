f581fe338981d2a1d180905d1b64bcbb5b855b32
Alesha <VAlesha@hq.telesens.lan>
2020-02-24 13:44:30 +0200

    addUser

--- src/users/user.router.js
+++ src/users/user.router.js
@@ -1,6 +1,7 @@
 const {
     getUser,
     authorization,
+    addUser
 } = require('./user.controller');
 const router = require("express").Router();
 const verifyInfo = require('../verifyInfo');
@@ -8,5 +9,7 @@ const verifyInfo = require('../verifyInfo');
 router.get('/user/getUser', getUser);
 
 router.post('/user/authorization', authorization);
+
+router.post('/user/addUser', addUser);
 
 module.exports = router;
