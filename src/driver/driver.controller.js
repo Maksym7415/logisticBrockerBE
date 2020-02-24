a77e429ca5569433f98a2e9814c8e5a235ab28b7
Dolhopolov, Kyrylo <k.dolhopolov@telesens.ua>
2020-02-24 17:02:46 +0200

    change driver's status api

--- src/driver/driver.controller.js
+++ src/driver/driver.controller.js
@@ -25,4 +25,18 @@ module.exports = {
             console.log(error);
         }
     },
+    changeDriverStatus: async (req, res, next) => {
+        console.log("aa");
+        try{
+            const promise = await seq.models.driver.update({
+                status:req.body.status},
+                {where:{
+                    id_driver:req.body.id,
+                }});
+            res.send(req.body.status);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
 }
--- src/driver/driver.router.js
+++ src/driver/driver.router.js
@@ -1,8 +1,11 @@
 const {
-    getDriver
+    getDriver,
+    changeDriverStatus
 } = require('./driver.controller');
 const router = require("express").Router();
 
 router.post('/driver/getDriver', getDriver);
+
+router.put('/driver/changeDriverStatus', changeDriverStatus);
 
 module.exports = router;
